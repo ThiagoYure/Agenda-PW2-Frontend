dfbbf8d974bf2f02a93f1528d9aead40a81c230d
ThiagoYure <thigoyure@gmail.com>
2021-09-05 13:31:43 -0300

    Configurações Heroku

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://agenda-pw2-backend.herokuapp.com/",
   headers: {'Content-Type': 'application/json'}
 });
 
